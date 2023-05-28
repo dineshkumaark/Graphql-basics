0ba5c014df40f63cd1e1e23465d2418cb4a23a48
Dineshkumaar <dineshkamara@gmail.com>
2023-05-28 21:39:47 +0530

    Intial Query fetch done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
+import { useEffect } from 'react';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
 import logo from './logo.svg';
+
 import './App.css';
 
+const client = new ApolloClient({
+   uri: 'http://localhost:4000',
+   cache: new InMemoryCache(),
+});
+
+const query = gql`
+   query {
+      allPersons {
+         name
+         phone
+         address {
+            street
+            city
+         }
+         id
+      }
+   }
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   useEffect(() => {
+      client.query({ query }).then((response) => {
+         console.log(response.data);
+      });
+   }, []);
+
+   return (
+      <div className='App'>
+         <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+               className='App-link'
+               href='https://reactjs.org'
+               target='_blank'
+               rel='noopener noreferrer'
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
